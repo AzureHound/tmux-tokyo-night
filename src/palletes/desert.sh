28d0e5bcd4561807631e74eb498f1a2f89ec6528
Wolf <183492338+AzureHound@users.noreply.github.com>
2025-04-24 00:58:36 +0530

    ✨ feat(tmux): desert theme

---
+++ src/palletes/desert.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+## FIX: Working Progress
+
+#Tokyo Night Desert Pallet
+declare -A PALLETE=(
+  [none]="NONE"
+  [bg]="#1a1b26"
+  [bg_dark]="#16161e"
+  [bg_highlight]="#292e42"
+  [terminal_black]="#414868"
+  [fg]="#c0caf5"
+  [fg_dark]="#a9b1d6"
+  [fg_gutter]="#3b4261"
+  [dark3]="#363a4f"
+  [comment]="#565f89"
+  [dark5]="#494d64"
+  [blue0]="#363a4f"
+  [blue]="#7aa2f7"
+  [cyan]="#7dcfff"
+  [blue1]="#2ac3de"
+  [blue2]="#0db9d7"
+  [blue5]="#89ddff"
+  [blue6]="#b4f9f8"
+  [blue7]="#363a4f"
+  [magenta]="#ed8796"
+  [magenta2]="#ff007c"
+  [purple]="#363a4f"
+  [orange]="#ff9e64"
+  [yellow]="#e5c890"
+  [green]="#a6da95"
+  [green1]="#73daca"
+  [green2]="#41a6b5"
+  [teal]="#1abc9c"
+  [red]="#f5a97f"
+  [red1]="#db4b4b"
+  [white]="#ffffff"
+)
+
+export PALLETE
